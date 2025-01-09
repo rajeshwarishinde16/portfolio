ef588e25c622169725bfa5dbbf8622c14090e3b8
rajeshwarishinde16 <shinderajeshwari439@gmail.com>
2025-01-09 23:53:38 +0530

    portfolio

---
+++ form.php
@@ -0,0 +1,43 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    
+    if (isset($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['message'])) {
+        
+       
+        $username = htmlspecialchars($_POST['name']);
+        $useremail = htmlspecialchars($_POST['email']);
+        $userphone = htmlspecialchars($_POST['phone']);
+        $usermessage = htmlspecialchars($_POST['message']);
+
+        /
+        $servername = "localhost";
+        $username_db = "root"; 
+        $password_db = "";    
+        $dbname = "portfolio"; 
+
+     
+        $conn = new mysqli($servername, $username_db, $password_db, $dbname);
+
+       
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        
+        $sql = "INSERT INTO Info (Name, Email, Phone, Message) VALUES ('$username', '$useremail', '$userphone', '$usermessage')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Data added successfully!";
+        } else {
+            echo "Error: " . $conn->error;
+        }
+       
+        $conn->close();
+    } else {
+     
+        echo "Please fill out all the fields.";
+    }
+}
+?>
